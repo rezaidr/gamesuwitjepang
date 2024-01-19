1e57c664dbccef7e47f9243daf61cf316c4cb664
rezaidr <rezaidrispurwanto@gmail.com>
2024-01-20 01:37:10 +0700

    second commit

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function getHasil(comp, player) {
   if (player == "rock") return comp == "paper" ? "KALAH" : "MENANG!";
 }
 
+// fungsi acak tangan/putar
 function putar() {
   const imgComputer = document.querySelector(".img-computer");
   const gambar = ["paper", "rock", "scissors"];
@@ -30,54 +31,66 @@ function putar() {
   }, 150);
 }
 
+// menangkap hasil
+let isKlikDiizinkan = true;
+
 const pilihan = document.querySelectorAll(".area-player img");
 pilihan.forEach(function (pilih) {
   pilih.addEventListener("click", function () {
-    const pilihanComp = getPilihanComputer();
-    const pilihanPlayer = pilih.className;
-    const hasil = getHasil(pilihanComp, pilihanPlayer);
-    const info = document.querySelector(".info");
-    const versus = document.querySelector(".versus");
-    versus.style.display = "inherit";
-    info.style.display = "none";
-
-    putar();
-
-    setTimeout(function () {
-      const imgComputer = document.querySelector(".img-computer");
-      imgComputer.setAttribute("src", "img/" + pilihanComp + ".jpg");
-    }, 1000);
-
-    setTimeout(function () {
-      const versus = document.querySelector(".versus");
+    if (isKlikDiizinkan) {
+      isKlikDiizinkan = false;
+      const pilihanComp = getPilihanComputer();
+      const pilihanPlayer = pilih.className;
+      const hasil = getHasil(pilihanComp, pilihanPlayer);
       const info = document.querySelector(".info");
-      info.innerHTML = hasil;
-      info.style.display = "inherit";
-      versus.style.display = "none";
-    }, 1500);
+      const versus = document.querySelector(".versus");
+      versus.style.display = "inherit";
+      info.style.display = "none";
 
-    setTimeout(function () {
-      const hadiah = document.querySelector(".hadiah");
-      const audioMenang = document.getElementById("menang-audio");
-      const audioKalah = document.getElementById("kalah-audio");
+      putar();
 
-      if (hasil == "MENANG!") {
-        hadiah.style.display = "flex";
-        audioMenang.play();
-      } else {
-        audioKalah.play();
-        audioKalah.currentTime = 0.4;
-      }
-    }, 2500);
+      setTimeout(function () {
+        const imgComputer = document.querySelector(".img-computer");
+        imgComputer.setAttribute("src", "img/" + pilihanComp + ".jpg");
+      }, 1000);
+
+      // mengubah vs menjadi info
+      setTimeout(function () {
+        const versus = document.querySelector(".versus");
+        const info = document.querySelector(".info");
+        info.innerHTML = hasil;
+        info.style.display = "inherit";
+        versus.style.display = "none";
+      }, 1500);
+
+      // memutar audio & memunculkan hadiah berdasarkan info
+      setTimeout(function () {
+        const hadiah = document.querySelector(".hadiah");
+        const audioMenang = document.getElementById("menang-audio");
+        const audioKalah = document.getElementById("kalah-audio");
+
+        if (hasil == "MENANG!") {
+          hadiah.style.display = "flex";
+          audioMenang.play();
+          isKlikDiizinkan = true;
+        } else {
+          audioKalah.play();
+          audioKalah.currentTime = 0.4;
+          isKlikDiizinkan = true;
+        }
+      }, 2500);
+    }
   });
 });
 
+// fungsi close button difitur hadiah
 const closeBtn = document.querySelector(".close-btn");
 closeBtn.addEventListener("click", function () {
   const hadiah = document.querySelector(".hadiah");
   hadiah.style.display = "none";
 });
 
+// fungsi tombol buka hadiah
 const bukaHadiahBtn = document.querySelector(".buka-hadiah");
 bukaHadiahBtn.addEventListener("click", function () {
   const prank = document.querySelector(".prank");
@@ -87,4 +100,16 @@ bukaHadiahBtn.addEventListener("click", function () {
   hadiah.style.display = "none";
   jumpscare.play();
   jumpscare.currentTime = 3;
+});
+
+// mouse hover pilihan player
+
+pilihan.forEach(function (pilih) {
+  pilih.addEventListener("mouseover", function () {
+    pilih.style.animation = "none";
+  });
+
+  pilih.addEventListener("mouseout", function () {
+    pilih.style.animation = "nyala 1s infinite";
+  });
 });
